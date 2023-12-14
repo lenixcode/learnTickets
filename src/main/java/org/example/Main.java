b28e220ea4ba8a0c54359111d387180f87c4988f
Leon <icedragon6222@gmail.com>
2023-12-14 23:05:40 +1200

    The game system has changed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,19 +4,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Выберите номер предмета:");
-        System.out.println(
-                "   1.Иностранный язык\n"+
-                "   2.Математический анализ\n"+
-                "   3.Линейная алгебра");
-
-        System.out.print("Куда пойдем: ");
-
-        int option = new Scanner(System.in).nextInt();
-        if(option != 1 ){
-            System.out.println("Простите, но он еще не готов:(");
-        }else{
             new Quiz().gameStart();
-        }
     }
 }
